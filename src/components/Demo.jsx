20771e755b0c32d06f88c1bfa029f8e6bd8f7681
Hanqi Zhang <hzhang526@gmail.com>
2024-09-16 12:30:16 +1000

    added aesthetic details and centering for demo

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,45 +1,57 @@
 import React from 'react'
 
-import {useState, useEffect} from 'react';
-import {copy, linkIcon, loader, tick} from '../assets'
+import { useState, useEffect } from 'react';
+import { copy, linkIcon, loader, tick } from '../assets'
 
 const Demo = () => {
-  return (
-    <section className="mt-16 w-full max-w-xl">
-        {/*Search*/}
-        <div className='flex flex-col w-full
-        gap-2'>
-            <form
-            className='relative flex
-            justify-center items-center'
-            onSubmit={()=>{}}
-            >
-                <img
-                src={linkIcon}
-                alt="link_icon"
-                className="absolute left-0 my-2
-                ml-3 w-5"
-                />
-                <input
-                    type="url"
-                    placeholder='enter a URL'
-                    value=""
-                    onChange={()=>{}}
-                    required
-                    className='url_input peer'
-                />
-                <button
-                type='submit'
-                className='sumbit_btn
-                peer-focus:border-gray-700
-                peer-focus:text-gray-700'
-                ></button>
-            </form>
+    const [article, setArticle] = useState({
+        url: '',
+        summary: '',
+    })
 
-        </div>
-        
-    </section>
-  )
+    const handleSubmit = async (e) => {
+        alert('submitted');
+    }
+
+    return (
+        <section className="mt-16 w-full max-w-xl">
+            {/*Search*/}
+            <div className='flex flex-col w-full gap-2'>
+                <form
+                className='relative flex
+                justify-center items-center'
+                onSubmit={handleSubmit}
+                >
+                    <img
+                    src={linkIcon}
+                    alt="link_icon"
+                    className="absolute left-0 my-2
+                    ml-3 w-5"
+                    />
+                    <input
+                        type="url"
+                        placeholder='enter a URL'
+                        value={article.url}
+                        onChange={(e) => setArticle({ ...
+                            article, url: e.target.value })}
+                        required
+                        className='url_input peer'
+                    />
+
+                    <button
+                        type='submit'
+                        className='sumbit_btn
+                    peer-focus:border-gray-700
+                    peer-focus:text-gray-700'
+                    >
+                    📄
+                    </button>
+                </form>
+
+            </div>
+
+        </section>
+    )
 }
 
 export default Demo
