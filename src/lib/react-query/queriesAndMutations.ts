2603be23011238e6560fce71f16c70b5c9a3b009
unknown <hzhang526@gmail.com>
2024-11-24 16:37:36 +1100

    fixed bugs in mutations by adding missing imports

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,20 +4,19 @@ import {
     useQueryClient,
     useInfiniteQuery,
 } from '@tanstack/react-query'
-import { createUserAccount } from '../appwrite/api'
+
+import { createUserAccount, signInAccount } from '../appwrite/api'
 import { INewUser } from '@/types'
 
 export const useCreateUserAccount = () => {
     return useMutation({
-        mutationFn: (user: INewUser) => createUserAccount(user)
-    })
-} 
+        mutationFn: (user: INewUser) => createUserAccount(user),
+      });
+};
 
 export const useSignInAccount = () => {
     return useMutation({
-        mutationFn: (user: {
-            email:string; 
-            password: string;
-        }) => createUserAccount(user)
-    })
-} 
+      mutationFn: (user: { email: string; password: string }) =>
+        signInAccount(user),
+    });
+  };
