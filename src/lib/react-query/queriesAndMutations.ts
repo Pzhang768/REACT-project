08e5100fd2c89cff62a5b4874a37a844ccbbe0e6
unknown <hzhang526@gmail.com>
2024-11-26 00:46:48 +1100

    modified topbar for signout links

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -9,14 +9,21 @@ import { createUserAccount, signInAccount } from '../appwrite/api'
 import { INewUser } from '@/types'
 
 export const useCreateUserAccount = () => {
-    return useMutation({
-      mutationFn: (user: INewUser) => createUserAccount(user),
-    });
-  };;
+  return useMutation({
+    mutationFn: (user: INewUser) => createUserAccount(user),
+  });
+};
 
 export const useSignInAccount = () => {
-    return useMutation({
-      mutationFn: (user: { email: string; password: string }) =>
-        signInAccount(user),
-    });
-  };
+  return useMutation({
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
+  });
+};
+
+export const useSignoutAccount = () => {
+  return useMutation({
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
+  });
+};
