19c2cfe5b11ec386db6920acb931a64a3d2c5125
unknown <hzhang526@gmail.com>
2024-11-29 00:42:45 +1100

    complete edit post function

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -11,7 +11,7 @@ import FileUploader from '../shared/FileUploader';
 import { PostValidation } from '@/lib/validation';
 import { useUserContext } from '@/context/AuthContext';
 import { useToast } from '@/hooks/use-toast';
-import { useCreatePost } from "@/lib/react-query/queriesAndMutations";
+import { useCreatePost, useUpdatePost } from "@/lib/react-query/queriesAndMutations";
 
 type PostFormProps = {
   post?: Models.Document,
@@ -19,7 +19,9 @@ type PostFormProps = {
 };
 
 const PostForm = ({ post, action }: PostFormProps) => {
+
   const { mutateAsync: createPost, isPending: isLoadingCreate} = useCreatePost();
+  const { mutateAsync: updatePost, isPending: isLoadingUpdate} = useUpdatePost();
   const navigate = useNavigate();
   const { toast } = useToast();
   const { user } = useUserContext();
@@ -34,6 +36,18 @@ const PostForm = ({ post, action }: PostFormProps) => {
   });
  
   async function onSubmit(values: z.infer<typeof PostValidation>) {
+    if(post && action == 'Update'){
+      const updatedPost = await updatePost({
+        ...values,
+        postId: post.$id,
+        imageId: post?.imageId,
+        imageUrl: post?.imageUrl
+      })
+      if(!updatedPost){
+        toast({ title: 'Please try again'})
+      }
+      return navigate(`/posts/${post.$id}`)
+    }
     const newPost = await createPost({
       ...values,
       userId: user.id
@@ -130,8 +144,10 @@ const PostForm = ({ post, action }: PostFormProps) => {
           <Button 
             type="submit"
             className='shad-button_primary whitespace-nowrap'
+            disabled={isLoadingCreate || isLoadingUpdate}
           >
-            Submit
+            {isLoadingCreate || isLoadingUpdate && 'Loading...'}
+            {action} post
           </Button>
         </div>
       </form>
